1d4d26bd84bdc47dad52334504ed7118018b177b
Marcos Lima <marcosanthony21@hotmail.com>
2020-05-15 21:46:43 -0300

    Criando input de login com componentes card e form-group

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
+// Importar script login.js como uma Classe
+import Login from './views/login'
+
 import 'bootswatch/dist/flatly/bootstrap.css'
 
 class App extends React.Component {
 
   render() {
     return (
-    <div>
-      <button type="button" className="btn btn-primary">Primary</button>
-
-    </div>
+      <div>
+        <Login />
+      </div>
     )
   }
 }
