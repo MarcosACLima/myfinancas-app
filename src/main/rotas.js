3462641b41d3cda47f18893cf2296ea128904291
Marcos Lima <marcosanthony21@hotmail.com>
2020-06-26 19:39:59 -0300

    Finalizando view de Home

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Login from '../views/login'
 import CadastroUsuario from '../views/cadastroUsuario'
+import Home from '../views/home'
 
 // importar os seguintes componentes
 import {Route, Switch, HashRouter} from 'react-router-dom'
@@ -12,6 +13,7 @@ function Rotas() {
             <Switch>
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuarios" component={CadastroUsuario} />
+                <Route path="/home" component={Home} />
             </Switch>
         </HashRouter>
     )
