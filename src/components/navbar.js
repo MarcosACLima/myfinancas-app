4fbdff58ddf92e4d567714ccb8cb693981482264
Marcos Lima <marcosanthony21@hotmail.com>
2020-07-18 19:56:27 -0300

    Menu toggler responsivo e mostrado somente apos autenticar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,13 +9,15 @@ function Navbar(props) {
         <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
             <div className="container">
                 <a href="#/home" className="navbar-brand">Minhas Finanças</a>
-                <button className="navbar-toggler" type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarResponsive"
-                    aria-controls="navbarResponsive" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                {props.isUsuarioAutenticado &&
+                    <button className="navbar-toggler" type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarResponsive"
+                        aria-controls="navbarResponsive" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                }
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav">
                         <NavbarItem href="#/home" label="Home" render={props.isUsuarioAutenticado} />
